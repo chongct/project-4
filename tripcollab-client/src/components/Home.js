26e3880a13b22591bbd767fd04dff6628c90b838
emjys <31798170+emjys@users.noreply.github.com>
2018-02-18 19:38:58 +0800

    updated pages

--- tripcollab-client/src/components/Home.js
+++ tripcollab-client/src/components/Home.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 import { Grid, Row, Col, Image, Button} from 'react-bootstrap';
 import '../styles/App.css';
 
+
 class Home extends Component {
   render() {
     return (
-      <Grid> 
-        <div class="about">
-          <h2>Welcome</h2>
-          <h3>Your Trip With Friends</h3>
+      <div>
+        <Image src="assets/friends.jpg" className="main-image" />
+          <h2>Welcome!</h2>
+          <h3>TripCollab can help to plan trip with your Friends</h3>
 
+          <Grid> 
           <Row className="show-grid text-center">
           <Col xs={12} sm={3} className="icon-wrapper">
             <Image src="assets/planning.svg" className="icon"/>
@@ -22,27 +24,29 @@ class Home extends Component {
           <Col xs={12} sm={3} className="icon-wrapper">
             <Image src="assets/calendar.svg" className="icon"/>
             <h4>Personalised Itinerary</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+            <p>Lorem ipsum dolor sit amet adipiscing elit. Aenean commodo ligula eget dolor.</p>
           </Col>
 
           <Col xs={12} sm={3} className="icon-wrapper">
             <Image src="assets/add-contact.svg" className="icon"/>
             <h4>Seamless Collaboration</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+            <p>Lorem ipsum dolor sit ame adipiscing elit. Aenean commodo ligula eget dolor.</p>
           </Col>
 
           <Col xs={12} sm={3} className="icon-wrapper">
             <Image src="assets/offline.svg" className="icon"/>
             <h4>Offline Access</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+            <p>Lorem ipsum dolor sit amet adipiscing elit. Aenean commodo ligula eget dolor.</p>
           </Col>
-          </Row>
+         
 
           <Link to="/Itinerary">
             <Button bsStyle="primary">Create Itinerary</Button>
           </Link>
-        </div>
+          </Row>
+        
       </Grid>
+      </div>
     )
   }
 }
